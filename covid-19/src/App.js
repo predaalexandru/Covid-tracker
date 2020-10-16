ae8a30b6c1115957bb2f3f819c62ff217b04b318
Preda Alexandru <alexpreda95@yahoo.com>
2020-10-16 18:23:44 +0300

    Creating Infoboxes

--- covid-19/src/App.js
+++ covid-19/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { MenuItem, FormControl, Select,} from "@material-ui/core";
 import './App.css';
+import InfoBox from "./components/InfoBoxes/InfoBox";
+
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   useEffect(() => {
     const getCountriesData = async () => {
@@ -24,27 +27,37 @@ function App() {
     getCountriesData();
   }, []);
 
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value;
+    setCountry(countryCode);
+  };
+
+
   return (
     <div className="app">
       {/* Header */}
+      {/* Title+Select input dropdown */}
       <div className="app__header">
         <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+          <MenuItem value="worldwide">Worldwide</MenuItem>
             {/* Loop through all the countries and show them in dropdown */}
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-            
           </Select>
         </FormControl>
       </div>
-     
-     {/* Title+Select input dropdown */}
 
-     {/* InfoBoxes */}
-     {/* InfoBoxes */}
-     {/* InfoBoxes */}
+      <div className="app__stats">
+          {/* InfoBoxs title=Coronavirus cases */}
+          <InfoBox title="Coronavirus Cases" cases={123} total={2000} />
+          {/* InfoBoxs title=Coronavirus recovers */}
+          <InfoBox title="Recovered" cases={1234} total={3000} />
+          {/* InfoBoxs title=Coronavirus deaths */}
+          <InfoBox title="Deaths" cases={12345} total={4000} />
+      </div>
 
      {/* Table */}
      {/* Graph */}
